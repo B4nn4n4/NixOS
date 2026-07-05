a3c4b2c83e808a2007865e31f07c9c771b799f74
B4nn4n4 <B4nn4n3ns4ft@gmail.com>
2026-07-05 23:32:07 +0200

    pinned freerdp to 3.26.0

--- flake.nix
+++ flake.nix
@@ -4,8 +4,11 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
+    # Only used for FreeRDP downgrade
+    nixpkgs-freerdp.url = "github:NixOS/nixpkgs/bd0ff2d3eac2";
+
     nixos-hardware.url = "github:NixOS/nixos-hardware/master";
-    
+
     home-manager = {
       url = "github:nix-community/home-manager/master";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -17,15 +20,32 @@
     };
   };
 
-  outputs = { self, nixpkgs, nixos-hardware, home-manager, forticlient-nixos, ... }:
+  outputs = {
+    self,
+    nixpkgs,
+    nixpkgs-freerdp,
+    nixos-hardware,
+    home-manager,
+    forticlient-nixos,
+    ...
+  }:
   {
     nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
 
       modules = [
+        # Overlay: use FreeRDP from older nixpkgs only
+        ({ pkgs, ... }: {
+          nixpkgs.overlays = [
+            (final: prev: {
+              freerdp = nixpkgs-freerdp.legacyPackages.${prev.system}.freerdp;
+            })
+          ];
+        })
+
         ./configuration.nix
         home-manager.nixosModules.home-manager
-	nixos-hardware.nixosModules.microsoft-surface-common
+        nixos-hardware.nixosModules.microsoft-surface-common
         forticlient-nixos.nixosModules.default
 
         {
