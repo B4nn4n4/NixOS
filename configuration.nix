6285668a1bd2460bac9919d22a7926048f9e98a1
B4nn4n4 <B4nn4n3ns4ft@gmail.com>
2026-08-03 22:59:11 +0200

    init qemu confiq

--- configuration.nix
+++ configuration.nix
@@ -72,6 +72,16 @@
   #Enable Graphics
   hardware.graphics.enable = true;
 
+  # QEMU / Virtualisation
+  security.polkit.enable = true;
+  virtualisation.libvirtd.enable = true;
+  virtualisation.libvirtd.qemu = {
+    package = pkgs.qemu_full;
+    runAsRoot = true;
+    swtpm.enable = true;
+  };
+  boot.kernelModules = [ "kvm-amd" "kvm-intel" "virtio" "virtio_pci" ];
+
   # Enable networking
   networking.networkmanager.enable = true;
 
@@ -108,6 +118,8 @@
     "networkmanager" "wheel"
     #Kanata
     "input" "uinput"
+    #Virtualisation
+    "libvirtd"
 
     ];
     packages = with pkgs; [];
@@ -156,12 +168,18 @@
 	blueman
 	pamixer
 	pavucontrol
+	#Virtualisation
+	virt-manager
+	virt-viewer
+	qemu
+	qemu_kvm
+	OVMF
 	#NixVim
 	ripgrep
 	fd
   ];
 
-  #Forticlient
+  #Forticlient-Gui
 programs.nix-ld = {
   enable = true;
   libraries = with pkgs; [
@@ -215,28 +233,5 @@ services.forticlient = {
 	defaultEditor = true;
   };
 
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
-
-  # List services that you want to enable:
-
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
-
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "25.11"; # Did you read the comment?
-
+   system.stateVersion = "25.11"; # Did you read the comment?
 }
